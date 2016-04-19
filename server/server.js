34bbc77abe5802628e124808017c7c481a7cb76d
Stephen Daniel Hughes <sdh31@duke.edu>
2016-04-19 19:38:47 -0400

    more

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var basic_db_utility = require('./basic_db_utility')
 var app = express();
 
-app.get('/', function (req, res) {
+
+app.get('/what', function (req, res) {
 
   var callback = function(result) {
     console.log(result);
@@ -13,6 +14,16 @@ app.get('/', function (req, res) {
     }
   };
   basic_db_utility.performMultipleRowDBOperation("SELECT * FROM a_;", callback);
+});
+
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function (req, res) {
+  res.render('/Users/stephenhughes/Dropbox/Duke/SeniorDuke/510-Research/index.html');
+});
+
+app.get('/chartDemo.js', function (req, res) {
+  res.sendFile('/Users/stephenhughes/Dropbox/Duke/SeniorDuke/510-Research/chartDemo.js');
 });
 
 app.listen(4000, function () {
