a3f9c611f47c8c0b38a34eda374979babfaf69e7
Stephen Daniel Hughes <sdh31@duke.edu>
2016-04-19 21:17:42 -0400

    more

--- chartDemo.js
+++ chartDemo.js
@@ -16,14 +16,16 @@ angular.module('chartDemo', ['chart.js'])
 
 function MainController($scope, $timeout, $http) {
 
+  $scope.selectedQueue = 'c';
+  $scope.queues = ['c', 'a1', 'a2', 'b1', 'b2', 'b3'];
 
-  $scope.queryDatabase = function() {
-    var query = '/data?field=absoluteUsedCapacity&table=b1_&start_time=0';
+
+  $scope.queryDatabase = function(field) {
+    var query = '/data?field=' + field + '&table=' + $scope.selectedQueue + '_' + '&start_time=0';
     return $http.get(query);
   };
 
-
-  $scope.buttonText = 'Start ';
+  $scope.buttonText = 'Collect Data';
   $scope.interactive = false;
   var vm = this;
 
@@ -32,17 +34,17 @@ function MainController($scope, $timeout, $http) {
 };
 
 
-  
-
-  var defaultData = [
-      [1]
-  	],
-    lineChart; //created in onCreate
+  var absoluteUsedCapacity = [[1]],
+      numPendingApplications = [[1]],
+      lineChart; //created in onCreate
   
   angular.extend(vm, {
-  	capacitySeries: ['Absolute Used Capacity'], 
-  	capacityData: defaultData,
-    capacityLabels: Object.keys(defaultData[0]),
+  	absoluteUsedCapacitySeries: ['Absolute Used Capacity'],
+    numPendingApplicationsSeries: ['Number of Pending Applications'],
+  	absoluteUsedCapacity: absoluteUsedCapacity,
+    absoluteUsedCapacityLabel: Object.keys(absoluteUsedCapacity[0]),
+    numPendingApplications: numPendingApplications,
+    numPendingApplicationsLabel: Object.keys(numPendingApplications[0]),
     options: options,
   	onClick: function (points, evt) {
     	
@@ -76,17 +78,31 @@ function MainController($scope, $timeout, $http) {
   function startTimer() {
     if (	vm.enable	) {
     	$timeout(function() {
-         $scope.queryDatabase().then(function(response) {
-            vm.capacityData[0] = response.data.values;
+         var count = 0;
+         $scope.queryDatabase('absoluteUsedCapacity').then(function(response) {
+            vm.absoluteUsedCapacity[0] = response.data.values;
             var len = response.data.values.length;
             var newLabs = [];
             for (var i = 0; i < len; i++) {
               newLabs.push('');
             }
 
-            vm.capacityLabels = newLabs
-            continueTimer();
+            vm.absoluteUsedCapacityLabel = newLabs;
          });
+
+        $scope.queryDatabase('numPendingApplications').then(function(response) {
+            vm.numPendingApplications[0] = response.data.values;
+            var len = response.data.values.length;
+            var newLabs = [];
+            for (var i = 0; i < len; i++) {
+              newLabs.push('');
+            }
+
+            vm.numPendingApplicationsLabel = newLabs;
+            count++;
+          });
+
+          continueTimer();
 
       	}, 1000);
     }
@@ -101,9 +117,9 @@ function MainController($scope, $timeout, $http) {
   	$scope.interactive = !$scope.interactive;
   	if ($scope.interactive) {
   		startTimer();
-  		$scope.buttonText = 'Stop ';
+  		$scope.buttonText = 'Stop Collecting Data';
   	} else {
-  		$scope.buttonText = 'Start ';
+  		$scope.buttonText = 'Collect Data';
   	}
 
   };
